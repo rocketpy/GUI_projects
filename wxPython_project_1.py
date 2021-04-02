1ffb29279266794e2bca0c9a40477ee391182e5b
rocketpy <dimitr.ushakov@gmail.com>
2021-04-02 23:37:49 +0300

    Update wxPython_project_1.py

--- wxPython_project_1.py
+++ wxPython_project_1.py
@@ -1,1 +1,12 @@
+import wx
 
+
+class MyFrame(wx.MDIParentFrame):
+    def __init__(self, parent, title):
+        super().__init__(parent, title=title)
+        
+app = wx.App()
+frame = MyFrame(None, "Project 1")
+frame.Show()
+app.MainLoop()
+        
