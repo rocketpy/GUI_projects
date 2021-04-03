11fea04692f126a2485439f75b05ff88144825ac
rocketpy <dimitr.ushakov@gmail.com>
2021-04-03 22:20:33 +0300

    Update wxPython_project_1.py

--- wxPython_project_1.py
+++ wxPython_project_1.py
@@ -8,6 +8,13 @@ class MyFrame(wx.MDIParentFrame):
         menu_bar = wx.MenuBar()
         f_menu = wx.Menu()
         item = f_menu.Append(wx.ID_EXIT, "Exit", "Exit from App")
+        menu_bar.Append(f_menu, "File")
+        self.SetMenuBar(menu_bar)
+        self.Bind(wx.EVT_MENU, self.exit_from_menu, item)
+        
+    def exit_from_menu(self, event):
+        self.Close()
+        
         
 app = wx.App()
 frame = MyFrame(None, "Project 1")
