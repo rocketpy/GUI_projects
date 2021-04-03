1e0550fed3560f3ff5158b7fc6e6edd3b6696d69
rocketpy <dimitr.ushakov@gmail.com>
2021-04-03 22:11:06 +0300

    Update wxPython_project_1.py

--- wxPython_project_1.py
+++ wxPython_project_1.py
@@ -5,6 +5,10 @@ class MyFrame(wx.MDIParentFrame):
     def __init__(self, parent, title):
         super().__init__(parent, title=title)
         
+        menu_bar = wx.MenuBar()
+        f_menu = wx.Menu()
+        item = f_menu.Append(wx.ID_EXIT, "Exit", "Exit from App")
+        
 app = wx.App()
 frame = MyFrame(None, "Project 1")
 frame.Show()
