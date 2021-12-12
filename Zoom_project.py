f06b7bd3ffacd49c21d4f4fd04141d546927b82a
rocketpy <dimitr.ushakov@gmail.com>
2021-12-12 21:53:14 +0200

    Update Zoom_project.py

--- Zoom_project.py
+++ Zoom_project.py
@@ -6,3 +6,11 @@ import subprocess
 import pyautogui
 
 
+def zoom_sign_in(id, password):
+    subprocess.call('C:\\Users\AppData\Roaming\Zoom\bin\Zoom.exe')
+    time.sleep(8)
+    join_button = pyautogui.locateCenterOnScreen('join_button.png')
+    pyautogui.moveTo(join_button)
+    pyautogui.click()
+
+
