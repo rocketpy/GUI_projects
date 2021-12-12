79f4be8c5c8547b38e14f8bf0973291637b2502b
rocketpy <dimitr.ushakov@gmail.com>
2021-12-12 22:05:17 +0200

    Update Zoom_project.py

--- Zoom_project.py
+++ Zoom_project.py
@@ -10,7 +10,20 @@ def zoom_sign_in(id, password):
     subprocess.call('C:\\Users\AppData\Roaming\Zoom\bin\Zoom.exe')
     time.sleep(8)
     join_button = pyautogui.locateCenterOnScreen('join_button.png')
+    
     pyautogui.moveTo(join_button)
     pyautogui.click()
+    
+    meeting_id_btn =  pyautogui.locateCenterOnScreen('meeting_id_button.png')
+    
+    pyautogui.click()
+    pyautogui.write(meetingid)
+
+    media_button = pyautogui.locateAllOnScreen('media_btn.png')
+    
+    for button in media_button:
+        pyautogui.moveTo(button)
+        pyautogui.click()
+        time.sleep(3)
 
 
