076d78082c26403d7ea20b9d9756ef87ea3013f4
rocketpy <dimitr.ushakov@gmail.com>
2022-02-12 22:35:40 +0200

    Update PySide2.py

--- PySide/PySide2.py
+++ PySide/PySide2.py
@@ -1,1 +1,9 @@
+#  PySide2 - Python bindings for the Qt cross-platform application and UI framework
+
+# PyPi: https://pypi.org/project/PySide2/
+# Docs: https://wiki.qt.io/Qt_for_Python
+# Quick start: https://doc.qt.io/qtforpython-5/quickstart.html
+
+# pip install PySide2
+
 
