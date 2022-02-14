dceb7c03a52f89ad3335d96aecbf8c160c7bc7ef
rocketpy <dimitr.ushakov@gmail.com>
2022-02-14 23:39:39 +0200

    Update PySide2.py

--- PySide/PySide2.py
+++ PySide/PySide2.py
@@ -31,6 +31,35 @@ print(PySide2.__version__)
 print(PySide2.QtCore.__version__)
 
 
+#  Create a Simple Application
+
+# Create a new file named app.py
+
+import sys
+import random
+from PySide2 import QtCore, QtWidgets, QtGui
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
 
 
 
