ad8f793c6faaa2868d246abb811c1a658a075b10
rocketpy <dimitr.ushakov@gmail.com>
2022-02-15 23:57:54 +0200

    Update PySide2.py

--- PySide/PySide2.py
+++ PySide/PySide2.py
@@ -84,4 +84,12 @@ ui.setupUi(Dialog)
 Dialog.show()
 sys.exit(app.exec_())
 
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
 
