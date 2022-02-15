20b3d6f8012f840799c5c2dd78a91fe2adcfa5df
rocketpy <dimitr.ushakov@gmail.com>
2022-02-15 23:51:28 +0200

    Update PySide2.py

--- PySide/PySide2.py
+++ PySide/PySide2.py
@@ -61,5 +61,13 @@ class MyWidget(QtWidgets.QWidget):
     def magic(self):
         self.text.setText(random.choice(self.hello))
 
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
 
 
