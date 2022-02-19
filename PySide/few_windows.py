6dddb4b4570dd885ee6186ba96f45358711b4112
rocketpy <dimitr.ushakov@gmail.com>
2022-02-19 23:07:29 +0200

    Update few_windows.py

--- PySide/few_windows.py
+++ PySide/few_windows.py
@@ -1,1 +1,13 @@
+import sys
+import random
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_t28(object):
+    def setupUi(self, t28):
+        self.verticalLayout = QtWidgets.QVBoxLayout(t28)
+        self.image_label = QtWidgets.QLabel("Ui_t28", t28)
+        self.verticalLayout.addWidget(self.image_label)
+        self.control_bt = QtWidgets.QPushButton("Next", t28)
+        self.verticalLayout.addWidget(self.control_bt)
 
