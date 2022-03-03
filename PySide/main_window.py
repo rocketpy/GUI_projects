2b95b1cb91e7ce57c322977e010f13f60350be33
rocketpy <dimitr.ushakov@gmail.com>
2022-03-03 23:57:23 +0200

    Update main_window.py

--- PySide/main_window.py
+++ PySide/main_window.py
@@ -1,1 +1,102 @@
+#  The QMainWindow class provides a main application window
 
+# Functions:
+"""
+
+    def addDockWidget (area, dockwidget)
+
+    def addDockWidget (area, dockwidget, orientation)
+
+    def addToolBar (area, toolbar)
+
+    def addToolBar (title)
+
+    def addToolBar (toolbar)
+
+    def addToolBarBreak ([area=Qt.TopToolBarArea])
+
+    def centralWidget ()
+
+    def corner (corner)
+
+    def dockOptions ()
+
+    def dockWidgetArea (dockwidget)
+
+    def documentMode ()
+
+    def iconSize ()
+
+    def insertToolBar (before, toolbar)
+
+    def insertToolBarBreak (before)
+
+    def isAnimated ()
+
+    def isDockNestingEnabled ()
+
+    def isSeparator (pos)
+
+    def menuBar ()
+
+    def menuWidget ()
+
+    def removeDockWidget (dockwidget)
+
+    def removeToolBar (toolbar)
+
+    def removeToolBarBreak (before)
+
+    def resizeDocks (docks, sizes, orientation)
+
+    def restoreDockWidget (dockwidget)
+
+    def restoreState (state[, version=0])
+
+    def saveState ([version=0])
+
+    def setCentralWidget (widget)
+
+    def setCorner (corner, area)
+
+    def setDockOptions (options)
+
+    def setDocumentMode (enabled)
+
+    def setIconSize (iconSize)
+
+    def setMenuBar (menubar)
+
+    def setMenuWidget (menubar)
+
+    def setStatusBar (statusbar)
+
+    def setTabPosition (areas, tabPosition)
+
+    def setTabShape (tabShape)
+
+    def setToolButtonStyle (toolButtonStyle)
+
+    def splitDockWidget (after, dockwidget, orientation)
+
+    def statusBar ()
+
+    def tabPosition (area)
+
+    def tabShape ()
+
+    def tabifiedDockWidgets (dockwidget)
+
+    def tabifyDockWidget (first, second)
+
+    def takeCentralWidget ()
+
+    def toolBarArea (toolbar)
+
+    def toolBarBreak (toolbar)
+
+    def toolButtonStyle ()
+
+    def unifiedTitleAndToolBarOnMac ()
+
+"""
