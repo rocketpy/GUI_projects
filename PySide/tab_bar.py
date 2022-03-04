d861f6f39718fbed1d1625315734d74781648ecb
rocketpy <dimitr.ushakov@gmail.com>
2022-03-04 23:25:38 +0200

    Update tab_bar.py

--- PySide/tab_bar.py
+++ PySide/tab_bar.py
@@ -1,1 +1,115 @@
+#  The QTabBar class provides a tab bar, e.g. for use in tabbed dialogs.
 
+# Functions
+"""
+    def accessibleTabName (index)
+
+    def addTab (icon, text)
+
+    def addTab (text)
+
+    def autoHide ()
+
+    def changeCurrentOnDrag ()
+
+    def count ()
+
+    def currentIndex ()
+
+    def documentMode ()
+
+    def drawBase ()
+
+    def elideMode ()
+
+    def expanding ()
+
+    def iconSize ()
+
+    def initStyleOption (option, tabIndex)
+
+    def insertTab (index, icon, text)
+
+    def insertTab (index, text)
+
+    def isMovable ()
+
+    def isTabEnabled (index)
+
+    def isTabVisible (index)
+
+    def moveTab (from, to)
+
+    def removeTab (index)
+
+    def selectionBehaviorOnRemove ()
+
+    def setAccessibleTabName (index, name)
+
+    def setAutoHide (hide)
+
+    def setChangeCurrentOnDrag (change)
+
+    def setDocumentMode (set)
+
+    def setDrawBase (drawTheBase)
+
+    def setElideMode (mode)
+
+    def setExpanding (enabled)
+
+    def setIconSize (size)
+
+    def setMovable (movable)
+
+    def setSelectionBehaviorOnRemove (behavior)
+
+    def setShape (shape)
+
+    def setTabButton (index, position, widget)
+
+    def setTabData (index, data)
+
+    def setTabEnabled (index, enabled)
+
+    def setTabIcon (index, icon)
+
+    def setTabText (index, text)
+
+    def setTabTextColor (index, color)
+
+    def setTabToolTip (index, tip)
+
+    def setTabVisible (index, visible)
+
+    def setTabWhatsThis (index, text)
+
+    def setTabsClosable (closable)
+
+    def setUsesScrollButtons (useButtons)
+
+    def shape ()
+
+    def tabAt (pos)
+
+    def tabButton (index, position)
+
+    def tabData (index)
+
+    def tabIcon (index)
+
+    def tabRect (index)
+
+    def tabText (index)
+
+    def tabTextColor (index)
+
+    def tabToolTip (index)
+
+    def tabWhatsThis (index)
+
+    def tabsClosable ()
+
+    def usesScrollButtons ()
+
+"""
