5c7a4d6d458693a2557a574196b3ab4c70b9694e
rocketpy <dimitr.ushakov@gmail.com>
2022-03-06 23:52:16 +0200

    Create menu.py

---
+++ PySide/menu.py
@@ -0,0 +1,95 @@
+# The QMenu class provides a menu widget for use in menu bars, context menus, and other popup menus.
+
+
+# Functions
+"""
+    def actionAt (arg__1)
+
+    def actionGeometry (arg__1)
+
+    def activeAction ()
+
+    def addAction (arg__1, arg__2, arg__3[, arg__4=0])
+
+    def addAction (arg__1, arg__2[, arg__3=0])
+
+    def addAction (icon, text)
+
+    def addAction (icon, text, receiver, member[, shortcut=0])
+
+    def addAction (text)
+
+    def addAction (text, receiver, member[, shortcut=0])
+
+    def addMenu (icon, title)
+
+    def addMenu (menu)
+
+    def addMenu (title)
+
+    def addSection (icon, text)
+
+    def addSection (text)
+
+    def addSeparator ()
+
+    def clear ()
+
+    def columnCount ()
+
+    def defaultAction ()
+
+    def exec_ ()
+
+    def exec_ (pos[, at=None])
+
+    def hideTearOffMenu ()
+
+    def icon ()
+
+    def initStyleOption (option, action)
+
+    def insertMenu (before, menu)
+
+    def insertSection (before, icon, text)
+
+    def insertSection (before, text)
+
+    def insertSeparator (before)
+
+    def isEmpty ()
+
+    def isTearOffEnabled ()
+
+    def isTearOffMenuVisible ()
+
+    def menuAction ()
+
+    def popup (pos[, at=None])
+
+    def separatorsCollapsible ()
+
+    def setActiveAction (act)
+
+    def setDefaultAction (arg__1)
+
+    def setIcon (icon)
+
+    def setSeparatorsCollapsible (collapse)
+
+    def setTearOffEnabled (arg__1)
+
+    def setTitle (title)
+
+    def setToolTipsVisible (visible)
+
+    def showTearOffMenu ()
+
+    def showTearOffMenu (pos)
+
+    def title ()
+
+    def toolTipsVisible ()
+
+"""
+
