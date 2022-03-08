5e2ca330052e023d32a4da71c06dc1753a2f104c
rocketpy <dimitr.ushakov@gmail.com>
2022-03-08 23:09:11 +0200

    Update application.py

--- PySide/application.py
+++ PySide/application.py
@@ -1,1 +1,16 @@
+#  The QApplication class manages the GUI application’s control flow and main settings.
+
+
+# Functions
+
+# def styleSheet ()
+
+# Slots
+"""
+    def autoSipEnabled ()
+
+    def setAutoSipEnabled (enabled)
+
+    def setStyleSheet (sheet)
+"""
 
