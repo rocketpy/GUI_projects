f4b31e65b67067fd56e6356145e9c78ca034c70a
rocketpy <dimitr.ushakov@gmail.com>
2022-03-08 23:07:16 +0200

    Update menu_bar.py

--- PySide/menu_bar.py
+++ PySide/menu_bar.py
@@ -1,1 +1,56 @@
+# The QMenuBar class provides a horizontal menu bar.
+
+
+# Functions
+"""
+    def actionAt (arg__1)
+
+    def actionGeometry (arg__1)
+
+    def activeAction ()
+
+    def addAction (arg__1, arg__2)
+
+    def addAction (text)
+
+    def addAction (text, receiver, member)
+
+    def addMenu (icon, title)
+
+    def addMenu (menu)
+
+    def addMenu (title)
+
+    def addSeparator ()
+
+    def clear ()
+
+    def cornerWidget ([corner=Qt.TopRightCorner])
+
+    def initStyleOption (option, action)
+
+    def insertMenu (before, menu)
+
+    def insertSeparator (before)
+
+    def isDefaultUp ()
+
+    def isNativeMenuBar ()
+
+    def setActiveAction (action)
+
+    def setCornerWidget (w[, corner=Qt.TopRightCorner])
+
+    def setDefaultUp (arg__1)
+
+    def setNativeMenuBar (nativeMenuBar)
+"""
+
+# Signals
+"""
+    def hovered (action)
+
+    def triggered (action)
+"""
+
 
