92fe8bcf89452b94d4f5c9297ff1129fc243c301
rocketpy <dimitr.ushakov@gmail.com>
2022-12-01 23:57:02 +0200

    Update ipywidgets.py

--- IPyWidgets/ipywidgets.py
+++ IPyWidgets/ipywidgets.py
@@ -4,3 +4,15 @@
 
 # pip:  pip install ipywidgets
 # conda:  conda install -c conda-forge ipywidgets
+
+# use widgets to build interactive GUIs for your notebooks
+
+import ipywidgets as widgets
+
+widgets.IntSlider()
+
+from IPython.display import display
+w = widgets.IntSlider()
+display(w)
+
+
