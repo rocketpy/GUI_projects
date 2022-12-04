bc2d30479ae681cd73185221c060059b3761168c
rocketpy <dimitr.ushakov@gmail.com>
2022-12-04 23:28:02 +0200

    Update ipywidgets.py

--- IPyWidgets/ipywidgets.py
+++ IPyWidgets/ipywidgets.py
@@ -43,3 +43,11 @@ b = widgets.FloatSlider()
 display(a,b)
 
 mylink = widgets.jslink((a, 'value'), (b, 'value'))
+
+
+# Unlinking widgets
+"""
+Unlinking the widgets is simple. All you have to do is call .unlink on the link object. 
+Try changing one of the widgets above after unlinking to see that they can be independently changed.
+"""
+# mylink.unlink()
