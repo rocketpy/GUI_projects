bfc072194610e8c039f793690755208333f426d3
rocketpy <dimitr.ushakov@gmail.com>
2022-12-04 23:26:52 +0200

    Update ipywidgets.py

--- IPyWidgets/ipywidgets.py
+++ IPyWidgets/ipywidgets.py
@@ -1,5 +1,8 @@
 # ipywidgets: Jupyter interactive widgets
 
+# https://github.com/jupyter-widgets/ipywidgets/
+# Docs: https://ipywidgets.readthedocs.io/en/latest/index.html
+
 # Install
 
 # pip:  pip install ipywidgets
@@ -29,3 +32,14 @@ w.value = 100
 # To see the entire list of synchronized, stateful properties of any specific widget, you can query the keys property.
 
 w.keys
+
+
+# Shorthand for setting the initial values of widget properties
+widgets.Text(value='Hello World!', disabled=True)
+
+# Linking two similar widgets
+a = widgets.FloatText()
+b = widgets.FloatSlider()
+display(a,b)
+
+mylink = widgets.jslink((a, 'value'), (b, 'value'))
